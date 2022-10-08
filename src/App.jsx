24861542421f6e2d2d597e10bb15ee3aa1b2284b
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-10-08 18:24:00 -0500

    To-do list change

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoSearch } from './components/TodoSearch';
 import { TodoList } from './components/TodoList';
 import { CreateTodoButton } from './components/CreateTodoButton';
 // import { AddTodoModal } from './components/AddTodoModal';
 import './App.css';
 
-const todos = [
+const defaultTodos = [
     { text: 'Cortar cebolla', completed: true },
-    { text: 'Cortar cebollas', completed: true },
-    { text: 'Cortar cebollassss', completed: true },
-    { text: 'Cortar cebollass', completed: true },
-    { text: 'Cortar cebollasss', completed: true },
-    { text: 'Tomar el cursso de intro a React', completed: false },
-    { text: 'Llorar con la llorona', completed: false },
-    { text: 'LALALALAA', completed: false },
+    { text: 'Terminar curso CodeStream', completed: true },
+    { text: 'Comprar una hamburguesa', completed: true },
+    { text: 'Terminar curso de Figma', completed: true },
+    { text: 'Terminar el curso de intro a React', completed: false },
+    { text: 'Sacar a pasear al perro', completed: false },
+    { text: 'Ir a comprar churros', completed: false },
+    { text: 'Preparar la cena', completed: false },
 ];
 
 function App() {
+    const [todos, setTodos] = useState(defaultTodos);
+    const [searchValue, setSearchValue] = useState('');
+
+    const completedTodos = todos.filter(todo => !!todo.completed).length;
+    let uncompletedTodos = todos.length - completedTodos;
+
     return (
         <div className='Container'>
             {/*<AddTodoModal />*/}
             <section className='App'>
                 <h1 className='Title'>MY TASKS</h1>
-                <TodoSearch />
+                <TodoSearch
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
 
                 <TodoList
                     title='Pendientes'
-                    count={5}
+                    count={uncompletedTodos}
                     todos={todos}
                     status={false}
+                    search={searchValue}
                 />
                 <TodoList
                     title='Completadas'
-                    count={2}
+                    count={completedTodos}
                     todos={todos}
                     status={true}
+                    search={searchValue}
                 />
 
                 <CreateTodoButton />
