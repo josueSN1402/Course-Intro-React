f283736adea935e2384c40f4d6fde43ea7bcf0e6
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-10-09 11:49:55 -0500

    Separation of the UI from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
-import { TodoSearch } from './components/TodoSearch';
-import { TodoList } from './components/TodoList';
-import { CreateTodoButton } from './components/CreateTodoButton';
-import { TodoItem } from './components/TodoItem';
-// import { AddTodoModal } from './components/AddTodoModal';
-import './styles/layout/App.css';
+import { AppUI } from './AppUI';
 
 const defaultTodos = [
     { text: 'Cortar cebolla', completed: true },
@@ -40,57 +35,17 @@ function App() {
 
     let getSearchValue = searchValue.toLowerCase();
 
-    const TaksList = props => {
-        return (
-            <TodoList
-                title={props.title}
-                count={props.count}
-                todos={todos}
-                status={props.status}
-                search={searchValue}
-            >
-                {todos
-                    .filter(t => t.text.toLowerCase().includes(getSearchValue))
-                    .map(
-                        todo =>
-                            todo.completed === props.status && (
-                                <TodoItem
-                                    key={todo.text}
-                                    text={todo.text}
-                                    completed={todo.completed}
-                                    onComplete={() => completeTodo(todo.text)}
-                                    onDelete={() => deleteTodo(todo.text)}
-                                />
-                            )
-                    )}
-            </TodoList>
-        );
-    };
-
     return (
-        <div className='Container'>
-            {/*<AddTodoModal />*/}
-            <section className='App'>
-                <h1 className='Title'>MY TASKS</h1>
-                <TodoSearch
-                    searchValue={searchValue}
-                    setSearchValue={setSearchValue}
-                />
-
-                <TaksList
-                    title='Pendientes'
-                    count={uncompletedTodos}
-                    status={false}
-                />
-                <TaksList
-                    title='Completadas'
-                    count={completedTodos}
-                    status={true}
-                />
-
-                <CreateTodoButton />
-            </section>
-        </div>
+        <AppUI
+            completedTodos={completedTodos}
+            uncompletedTodos={uncompletedTodos}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            completeTodo={completeTodo}
+            deleteTodo={deleteTodo}
+            todos={todos}
+            getSearchValue={getSearchValue}
+        />
     );
 }
 
---
+++ src/AppUI.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoList } from './components/TodoList';
+import { CreateTodoButton } from './components/CreateTodoButton';
+import { TodoItem } from './components/TodoItem';
+// import { AddTodoModal } from './components/AddTodoModal';
+import './styles/layout/App.css';
+
+function AppUI({
+    completedTodos,
+    uncompletedTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    todos,
+    getSearchValue,
+}) {
+    const TaksList = props => {
+        return (
+            <TodoList
+                title={props.title}
+                count={props.count}
+                todos={todos}
+                status={props.status}
+                search={searchValue}
+            >
+                {todos
+                    .filter(t => t.text.toLowerCase().includes(getSearchValue))
+                    .map(
+                        todo =>
+                            todo.completed === props.status && (
+                                <TodoItem
+                                    key={todo.text}
+                                    text={todo.text}
+                                    completed={todo.completed}
+                                    onComplete={() => completeTodo(todo.text)}
+                                    onDelete={() => deleteTodo(todo.text)}
+                                />
+                            )
+                    )}
+            </TodoList>
+        );
+    };
+
+    return (
+        <div className='Container'>
+            {/*<AddTodoModal />*/}
+            <section className='App'>
+                <h1 className='Title'>MY TASKS</h1>
+                <TodoSearch
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
+
+                <TaksList
+                    title='Pendientes'
+                    count={uncompletedTodos}
+                    status={false}
+                />
+                <TaksList
+                    title='Completadas'
+                    count={completedTodos}
+                    status={true}
+                />
+
+                <CreateTodoButton />
+            </section>
+        </div>
+    );
+}
+
+export { AppUI };
